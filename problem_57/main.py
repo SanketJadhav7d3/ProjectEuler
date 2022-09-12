d493389583949d3a4b8d4e0b20c487f88aff41ff
SanketJadhav7d3 <lonewolf7d3@gmail.com>
2022-09-12 11:57:28 +0530

    problem 57

---
+++ problem_57/main.py
@@ -0,0 +1,15 @@
+
+
+import math
+from fractions import Fraction
+    
+
+def construct(n):
+    for i in range(n):
+        pass
+
+
+if __name__ == "__main__":
+    d = Fraction(2) + Fraction(1, 2)
+
+    print(d)
